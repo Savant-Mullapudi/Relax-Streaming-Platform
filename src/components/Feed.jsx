8c700ff2b44b4a8599dae0d8891aa7b435a9bedc
Savant Mullapudi <96577546+Savant-Mullapudi@users.noreply.github.com>
2025-05-30 20:34:54 -0600

    Update Feed.jsx
    Signed-off-by: Savant Mullapudi <96577546+Savant-Mullapudi@users.noreply.github.com>

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -9,19 +9,21 @@ const Feed = () => {
     const [videos, setVideos] = useState([]);
 
     useEffect(() => {
-        // Correct template literal for API request
+
         fetchfromAPI(`search?part=snippet&q=${selectedCategory}`)
             .then((data) => setVideos(data.items))
-            .catch((error) => console.error('Error fetching data:', error)); // Handle potential errors
+            .catch((error) => console.error('Error fetching data:', error)); 
     }, [selectedCategory]);
 
     return (
         <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+            
             <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
                 <SideBar selectedCategory={selectedCategory} setselectedCategory={setselectedCategory} />
                 <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
                 </Typography>
             </Box>
+            
             <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
                 <Typography
                     variant="h4"
@@ -33,6 +35,7 @@ const Feed = () => {
                 </Typography>
                 <Videos videos={videos} />
             </Box>
+            
         </Stack>
     );
 };
